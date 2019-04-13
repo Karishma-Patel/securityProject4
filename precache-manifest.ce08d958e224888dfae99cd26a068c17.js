e14bacca4954ffa5a48def3d3d17638385dbc6d5
Regina Chen <regina.y.chen@utexas.edu>
2019-04-13 10:37:33 -0500

    Updates

--- precache-manifest.ce08d958e224888dfae99cd26a068c17.js
+++ precache-manifest.ce08d958e224888dfae99cd26a068c17.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/Karishma-Patel/securityProject4/static/js/runtime~main.8bc0ccd5.js"
   },
   {
-    "revision": "296778b284fc9e7d5653",
-    "url": "/Karishma-Patel/securityProject4/static/js/main.f23bb42a.chunk.js"
+    "revision": "a79b23e67f83dccffcd2",
+    "url": "/Karishma-Patel/securityProject4/static/js/main.351db69e.chunk.js"
   },
   {
-    "revision": "bcc50879540ad49dcc55",
-    "url": "/Karishma-Patel/securityProject4/static/js/2.31d846fc.chunk.js"
+    "revision": "7a19b859ef222a1903e4",
+    "url": "/Karishma-Patel/securityProject4/static/js/2.c75d78a8.chunk.js"
   },
   {
-    "revision": "296778b284fc9e7d5653",
+    "revision": "a79b23e67f83dccffcd2",
     "url": "/Karishma-Patel/securityProject4/static/css/main.2f7324f6.chunk.css"
   },
   {
-    "revision": "e41412196e501c8818cb4d246a8fbe75",
+    "revision": "37f898a7a2028e33180fb1179b6aeb7d",
     "url": "/Karishma-Patel/securityProject4/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Karishma-Patel/securityProject4/precache-manifest.cb0138698e5ef49cf88536910e237150.js"
+  "/Karishma-Patel/securityProject4/precache-manifest.ce08d958e224888dfae99cd26a068c17.js"
 );
 
 workbox.clientsClaim();
